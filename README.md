494b9f00dcbb696a64159af2fdf374887024a764
Chris Tornatta <chris@tornatta.net>
2017-03-19 20:13:54 -0400

    Added arguments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ocp-template-patch
+
+Example Use:
+```
+/template_patch.py ocp_mysql_template.json ocp_mysql_template_mod.json
+```
--- template_patch.py
+++ template_patch.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python
 import json
+import argparse
 from collections import OrderedDict
 
+
 claim_label = {
-                "selector": {
-                     "matchLabels": {
-                        "environment": "internal"
+                    "selector": {
+                        "matchLabels": {
+                            "environment": "internal"
+                        }
                     }
                 }
-              }
 
-with open('ocp_mysql_template.json') as json_file:
+def get_args():
 
-    # load json file and keep the order
-    data = json.load(json_file, object_pairs_hook=OrderedDict)
+    parser = argparse.ArgumentParser(description='Appends a selector to the persistent volume claim in the template')
+    parser.add_argument("infile", type=str, help='Template file to read in. Must be a JSON file')
+    parser.add_argument("outfile", type=str, help='The modified template file to write to. Will be a JSON file')
+    parser.add_argument('--debug', action='store_true', help='Enable debugging output')
 
-    # Since 'objects' is a list we need to get the index
-    for index, kind_dict in enumerate(data['objects']):
-         if 'PersistentVolumeClaim' in kind_dict['kind']:
+    args = parser.parse_args()
 
-            # Update the 'spec' dictionary with the claim label
-            data['objects'][index]['spec'].update(claim_label)
+    return args
+
+if __name__ == '__main__':
+
+    args = get_args()
+
+    with open(args.infile) as json_file:
+        # load json file and keep the order
+        data = json.load(json_file, object_pairs_hook=OrderedDict)
+
+        # Since 'objects' is a list we need to get the index
+        for index, kind_dict in enumerate(data['objects']):
+             if 'PersistentVolumeClaim' in kind_dict['kind']:
+
+                # Update the 'spec' dictionary with the claim label
+                data['objects'][index]['spec'].update(claim_label)
 
 
-with open('out.txt', 'w') as outfile:
-    json.dump(data, outfile,indent=4)
+    with open(args.outfile, 'w') as outfile:
+        json.dump(data, outfile,indent=4)
 
 # vim: ai et ts=4 sts=4 sw=4 nu
