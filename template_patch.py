08b8268151fb4487dda9f123c9b5bc1c4b7dcbd8
Chris Tornatta <chris@tornatta.net>
2017-03-20 17:37:49 -0400

    Added 'debug' and 'label' arguments

--- template_patch.py
+++ template_patch.py
@@ -3,24 +3,14 @@ import json
 import argparse
 from collections import OrderedDict
 
-
-claim_label = {
-                    "selector": {
-                        "matchLabels": {
-                            "environment": "internal"
-                        }
-                    }
-                }
-
 def get_args():
 
     parser = argparse.ArgumentParser(description='Appends a selector to the persistent volume claim in the template')
     parser.add_argument("infile", type=str, help='Template file to read in. Must be a JSON file')
     parser.add_argument("outfile", type=str, help='The modified template file to write to. Will be a JSON file')
+    parser.add_argument("label", type=str, help='The selector label that you want to apply')
     parser.add_argument('--debug', action='store_true', help='Enable debugging output')
-
     args = parser.parse_args()
-
     return args
 
 if __name__ == '__main__':
@@ -34,10 +24,20 @@ if __name__ == '__main__':
         # Since 'objects' is a list we need to get the index
         for index, kind_dict in enumerate(data['objects']):
              if 'PersistentVolumeClaim' in kind_dict['kind']:
+                # Apply the label argument to the string and convert it to JSON
+                claim_label_string = '{{"selector":{{"matchLabels": {{"environment": "{}"}}}}}}'.format(args.label)
+                claim_label = json.loads(claim_label_string)
+
+                if args.debug:
+                    print('-' * 20)
+                    print('INPUT FILE: {}'.format(args.infile))
+                    print('OUTPUT FILE: {}'.format(args.outfile))
+                    print('LABEL TO APPLY: {}'.format(args.label))
+                    print('CLAIM LABEL STRING: {}'.format(claim_label_string))
+                    print('-' * 20)
 
                 # Update the 'spec' dictionary with the claim label
                 data['objects'][index]['spec'].update(claim_label)
-
 
     with open(args.outfile, 'w') as outfile:
         json.dump(data, outfile,indent=4)
